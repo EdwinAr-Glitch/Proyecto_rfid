39d1896064df0a765f75151cf6d7a6fa59c67c02
Edwin Arias <edwinarias2911@gmail.com>
2025-05-12 03:55:08 -0600

    inicio archivo gestor db

--- gestor_db.py
+++ gestor_db.py
@@ -5,3 +5,28 @@ from datetime import datetime
 
 DB_NAME = 'rfid_data.db'
 
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('''
+         CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre TEXT NOT NULL, 
+                   uid TEXT UNIQUE NOT NULL,
+                   tipo TEXT
+                   )
+                ''')
+    cursor.executeU('''
+                    CREATE TABLE IF NOT EXISTS accesos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre TEXT NOT NULL,
+                    uid TEXT UNIQUE NOT NULL,
+                    fecha TEXT NOT NULL,
+                    tipo TEXT,
+                    lector TEXT
+                    )
+                ''')
+    conn.commit()
+    conn.close()
+
+    #def validar_uid(uid):
+   #     conn = sqlite3.connect(DB_NAME)
