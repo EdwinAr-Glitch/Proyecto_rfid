353c9815e18c311b544fd5c042d571e5055578bf
Edwin Arias <edwinarias2911@gmail.com>
2025-05-17 12:13:30 -0600

    Proyecto rfid continuacion

--- gestor_db.py
+++ gestor_db.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 DB_NAME = 'rfid_data.db'
 
+# Crear las tablas si no existen
 def init_db():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
@@ -15,7 +16,7 @@ def init_db():
                    tipo TEXT
                    )
                 ''')
-    cursor.executeU('''
+    cursor.execute('''
                     CREATE TABLE IF NOT EXISTS accesos (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nombre TEXT NOT NULL,
@@ -28,5 +29,36 @@ def init_db():
     conn.commit()
     conn.close()
 
-    #def validar_uid(uid):
-   #     conn = sqlite3.connect(DB_NAME)
+# Validar si el UID está registrado
+def validar_uid(uid):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('SELECT nombre FROM usuarios WHERE uid = ?',(uid,))
+    resultado = cursor.fetchone()
+    conn.close
+    if resultado:
+        return resultado[0] #retorna un nombre si existe
+    else:
+        return None
+
+
+# Registrar un acceso si el UID es válido# Registrar un acceso si el UID es válido
+def registrar_acceso(uid, lector):
+    nombre = validar_uid(uid)
+    if nombre:
+        conn = sqlite3.connect(DB_NAME)
+        cursor = conn.cursor()
+        fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('INSERT INTO accesos (uid, fecha, tipo, lector) VALUES (?,?,?,?)',
+                       (uid,fecha,"Entrada",lector))
+        
+        conn.commit()
+        conn.close()
+        print(f"[ACCESO] {nombre} ({uid}) - {fecha} - {lector}")
+        return True
+    else:
+        print(f"[DENEGADO] tarjeta no registrada")
+        return False
+if __name__ == "__main__":
+    init_db()
+    print("[INFO] Base de datos inicializada")
